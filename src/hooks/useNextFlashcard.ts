07d6beb253057236f22cfe62508d60edf83a9ecb
Kamilbcg <benkirane.kamil@bcg.com>
2024-02-06 19:28:52 +0100

    commi pour Nassim

--- src/hooks/useNextFlashcard.ts
+++ src/hooks/useNextFlashcard.ts
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 
-const useNextFlashcard = (userId, activeDecksIds) => {
+const useNextFlashcard = (userId, activeDecksIds, triggerFetch) => {
     const [nextFlashcard, setNextFlashcard] = useState(null);
 
     useEffect(() => {
+        console.log("activeDecksIds", triggerFetch)
         const queryParams = activeDecksIds.map(id => `active_decks_id=${id}`).join('&');
         const url = `http://127.0.0.1:5000/flashcard/next_card/${userId}?${queryParams}`;
 
