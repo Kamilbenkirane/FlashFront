5b33816be5da3555d52cae40daaa16213f4dc08a
Kamilbcg <benkirane.kamil@bcg.com>
2024-02-14 11:42:49 +0100

    Formatting

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // const API_URL = 'http://127.0.0.1:5000';
-const API_URL = 'http://192.168.1.92:5000'; // WIFI
+const API_URL = 'http://192.168.1.92:5000' // WIFI
 // const API_URL = 'https://8027-88-183-137-163.ngrok-free.app
-export default API_URL;
+export default API_URL
