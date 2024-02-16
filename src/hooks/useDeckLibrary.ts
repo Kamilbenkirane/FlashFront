cb5c8dd4a2a0c4726a2f35e68da14b59fd7d9069
Kamilbcg <benkirane.kamil@bcg.com>
2024-02-16 18:46:15 +0100

    Some bugs !

--- src/hooks/useDeckLibrary.ts
+++ src/hooks/useDeckLibrary.ts
@@ -2,28 +2,25 @@ import { useEffect, useState } from 'react';
 import API_URL from '../config';
 
 type Deck = {
-    id: string;
-    deck_name: string;
-    subject: string;
-    // include other properties as needed
-  };
-
+  id: string;
+  deck_name: string;
+  subject: string;
+  // include other properties as needed
+};
 
 const useDeckLibrary = (): Deck[] => {
-    const [decks, setDecks] = useState([]);
+  const [decks, setDecks] = useState([]);
 
-    useEffect(() => {
-        
-        const fetchDecks = async () => {
-            const response = await fetch(`${API_URL}/deck/list/`); // Adjust endpoint as necessary
-            const data = await response.json();
-            setDecks(data);
-        };
-            fetchDecks();
-        
-    }, );
+  useEffect(() => {
+    const fetchDecks = async () => {
+      const response = await fetch(`${API_URL}/deck/list/`); // Adjust endpoint as necessary
+      const data = await response.json();
+      setDecks(data);
+    };
+    fetchDecks();
+  });
 
-    return decks;
+  return decks;
 };
 
 export default useDeckLibrary;
