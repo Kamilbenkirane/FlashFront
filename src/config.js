ef57fd2f6b3367cde8b5afe3e77132e03733418a
Kamilbcg <benkirane.kamil@bcg.com>
2024-02-28 10:48:09 +0100

    Added edit button and multiple fixes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-//const API_URL = 'http://127.0.0.1:5000';
+// const API_URL = 'http://127.0.0.1:5000';
 // const API_URL = 'http://192.168.1.92:5000'; // WIFI
 // const API_URL = 'http://192.168.2.63:5000'; // WIFI PARENTS
 // const API_URL = 'https://8027-88-183-137-163.ngrok-free.app
